9cc828e76ac2ca4ae3d77e1e5d07d80896161ad5
Digital Bacon <crispin.anderton@gmail.com>
2018-08-11 15:43:30 +1200

    Re-organised assets; conveyor belt
    Reorganised assets so that all textures used are in the Assets/Textures folder (not any of the subfolders).
    
    Added a conveyor belt and animated it programmatically.

---
+++ Assets/Scripts/Conveyor_Belt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor_Belt : MonoBehaviour {
+    private readonly float animationSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimateConveyorBelt();
+	}
+
+    private void AnimateConveyorBelt() {
+        Debug.Log("AnimateConveyorBelt");
+        Vector2 offset = gameObject.GetComponent<Renderer>().material.mainTextureOffset;
+        float new_offset = offset.x + (animationSpeed * Time.deltaTime);
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(new_offset, offset.y);
+    }
+}
