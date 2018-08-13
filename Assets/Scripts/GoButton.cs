ccc28d2b8c766a3408df32f74281f2b85f3528bb
Digital Bacon <crispin.anderton@gmail.com>
2018-08-13 23:47:44 +1200

    Added Welcome Screen

---
+++ Assets/Scripts/GoButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoButton : MonoBehaviour {
+    [SerializeField] private Button goButton;
+    private AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = goButton.GetComponent<Button>();
+        btn.onClick.AddListener(StartGame);
+        music = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void StartGame () {
+        music.Stop();
+        SceneManager.LoadScene("Game");
+	}
+}
